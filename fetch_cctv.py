821bd9598ad033eb351309413fe1ea858f33eba1
RobertChenTW <iotrobchen@gmail.com>
2026-04-26 18:37:45 +0800

    Update fetch_cctv.py

--- fetch_cctv.py
+++ fetch_cctv.py
@@ -14,7 +14,7 @@ token = auth_res.json().get('access_token')
 
 # 2. 呼叫 API 抓取鳶山堰影像 (透過名稱過濾)
 # api_url = "https://tdx.transportdata.tw/api/basic/v2/Resources/CCTV?$filter=contains(CCTVName,'鳶山堰')&$format=JSON"
-api_url = "https://tdx.transportdata.tw/api/basic/v2/Resources/CCTV?$filter=AuthorityCode eq 'WRA'&$format=JSON"
+api_url = "api_url = "https://tdx.transportdata.tw/api/basic/v2/Resource/CCTV?$filter=AuthorityCode%20eq%20'WRA'&$format=JSON""
 
 headers = {'Authorization': f'Bearer {token}'}
 api_res = requests.get(api_url, headers=headers)
